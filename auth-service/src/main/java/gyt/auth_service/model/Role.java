66f88854da2a9166986292f50522810946b1e4d2
sintayehu-dev <sintayehubelfa23@gmail.com>
2025-12-25 23:15:27 -0800

    feat: Add `User` entity and `Role` enum for authentication models.

---
+++ auth-service/src/main/java/gyt/auth_service/model/Role.java
@@ -0,0 +1,6 @@
+package gyt.auth_service.model;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ auth-service/src/main/java/gyt/auth_service/model/User.java
@@ -0,0 +1,30 @@
+package gyt.auth_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
